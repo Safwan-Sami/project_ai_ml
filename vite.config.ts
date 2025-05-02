f0545c7f9d8ed6a9c65e30011b0d3a341e2dff03
Safwan-Sami <102474482+Safwan-Sami@users.noreply.github.com>
2025-05-03 03:29:44 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/project_ai_ml/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
